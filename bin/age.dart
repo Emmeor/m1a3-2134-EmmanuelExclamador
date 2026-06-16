40d1a2a19d2c3d200ad6687f178a1fff73f0b28e
Emmeor <emmanuelexclamadormendoza@gmail.com>
2026-06-17 01:46:01 +0800

    Add user input for nickname and age calculation

--- bin/age.dart
+++ bin/age.dart
@@ -9,6 +9,17 @@
 // Concepts to research (see the Dart cheat sheet): reading input from the
 // user, converting text into a number, and printing to the console.
 
+import 'dart:io';
+
 void main() {
   // TODO: write your solution here
+  stdout.write("Enter your nickname: ");
+  String nickname = stdin.readLineSync()!;
+
+  stdout.write("Enter your current age: ");
+  int age = int.parse(stdin.readLineSync()!);
+
+  int yearsLeft = 100 - age;
+
+  print("$nickname, you have $yearsLeft years left before you reach 100.");
 }
